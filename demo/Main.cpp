f0f21ead428b5b14eeebf3bdef89f9844c123a32
Dimo Markov <dimomarkov@gmail.com>
2024-06-29 21:29:30 +0300

    Testing out movement anticipators

--- demo/Main.cpp
+++ demo/Main.cpp
@@ -49,7 +49,7 @@ int main(int, char**) {
    player->CreateUnits<A::Camera, A::InputListener>();
    player->CreateUnit<A::Instance>(Traits::Place {0, 20, 20});
    player->Run("Create(Anticipator(MouseMove, [thing? Move*-1 (Yaw(?.x * 0.05), Pitch(?.y * 0.05))]))");
-   //player->Run("Create(Anticipator(Keys::W,   [thing? Move    (Forward * Derive(?.Time))]))");
+   player->Run("Create(Anticipator(Keys::W,   [thing? Move    (Axes::Forward * Derive(?.Time))]))");
    player->Run("Move Pitch(-45)");
 
    // Create a castle                                                   
