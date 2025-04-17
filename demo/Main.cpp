c0fb45dd3ae650d5226de4c13b7968f6d2c5fd69
Epixu <dimomarkov@gmail.com>
2025-04-17 19:21:56 +0300

    Rotating the light (WIP)

--- demo/Main.cpp
+++ demo/Main.cpp
@@ -66,7 +66,8 @@ int main(int, char**) {
    // Create a directional light source                                 
    auto sun = root.CreateChild("Sun");
    sun->CreateUnits<A::Light>();
-   sun->CreateUnit<A::Instance>(Traits::Place {-10, -50, 0}, Traits::Aim {-1, -1, 0});
+   sun->CreateUnit<A::Instance>(Traits::Aim {-1, -1, 0});
+   sun->Run("? move^1 (Yaw(1), relative)");
 
    // Loop until quit                                                   
    while (root.Update(fps.GetDeltaTime()))
